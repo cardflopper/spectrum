c69fb10d8963e540da6346438201e8fda3633e1c
Shawn Chacko <Shawn.Chacko@trca.ca>
2024-03-13 02:10:11 -0400

    fix target random value

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ function newRound(){
     drawCard();
     var t = document.getElementById("theTarget");
     t.classList = "";
-    t.innerText = randomInt(100)+1;
+    t.innerText = randomInt(101);
     var e = document.getElementsByClassName("eye");
     e[0].classList.remove("green");
 }
@@ -14,13 +14,14 @@ function drawCard(){
 
     var blocks = document.getElementsByClassName("block");
 
-    blocks[1].innerText = card[0];
-    blocks[0].innerText = card[1];
+    blocks[0].innerText = card[0];
+    blocks[1].innerText = card[1];
 }
 
 newRound();
 
 function toggleHide(){
+    
     var t = document.getElementById("theTarget");
     t.classList.toggle("hidden");
     var e = document.getElementsByClassName("eye");
